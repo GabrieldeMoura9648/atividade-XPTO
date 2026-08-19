e414f475d8894951705b0ed20b1a5f54965a8344
Gabriel de Moura <gabriel.9648moura@gmail.com>
2026-08-19 13:42:33 +0000

    Adiciona a pasta .github, pipelines e a calculadora em python

---
+++ teste_app.py
@@ -0,0 +1,17 @@
+from calculadora import soma, subtracao, multiplicacao, divisao
+
+
+def test_soma():
+    assert soma(10, 5) == 15
+
+
+def test_subtracao():
+    assert subtracao(10, 5) == 5
+
+
+def test_multiplicacao():
+    assert multiplicacao(10, 5) == 50
+
+
+def test_divisao():
+    assert divisao(10, 5) == 2
